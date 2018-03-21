d0d24535b7f40a5719ca438a33cd8399aebd549d
felwoord <felwoord@hotmail.com>
2018-03-21 18:23:34 -0300

    12.5

---
+++ Assets/Scripts/HideHint.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideHint : MonoBehaviour {
+	private float timer;
+	// Use this for initialization
+	void Start () {
+		timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer > 3) {
+			if (gameObject.name == "PanelStr") {
+				GameObject.Find ("Main Camera").GetComponent<ShopMenu> ().StrFloatingHint ();
+			}
+			if (gameObject.name == "PanelMagic") {
+				GameObject.Find ("Main Camera").GetComponent<ShopMenu> ().MagicFloatingHint ();
+			}
+			if (gameObject.name == "PanelVit") {
+				GameObject.Find ("Main Camera").GetComponent<ShopMenu> ().VitFloatingHint ();
+			}
+		}
+	}
+}
