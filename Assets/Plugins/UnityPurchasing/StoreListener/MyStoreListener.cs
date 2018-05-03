b8c6188f1334abb394b63992e47bbd991259d4bf
felwoord <felwoord@hotmail.com>
2018-05-02 23:09:17 -0300

    33.2

---
+++ Assets/Plugins/UnityPurchasing/StoreListener/MyStoreListener.cs
@@ -0,0 +1,40 @@
+﻿//using System;
+//using UnityEngine;
+//using UnityEngine.Purchasing;
+//
+//public class MyStoreListener : MonoBehaviour, IStoreListener
+//{
+//	void Awake()
+//	{
+//		//ConfigurationBuilder builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
+//		//UnityPurchasing.Initialize(this, builder);
+//	}
+//
+//	public void OnInitializeFailed(InitializationFailureReason error) {
+//		
+//	}
+//
+//	public void OnPurchaseFailed(Product item, PurchaseFailureReason r) {
+//		
+//	}
+//		
+//	public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs e) {
+//		Debug.Log ("item comprado: "+e.purchasedProduct.definition.id);
+//		return PurchaseProcessingResult.Complete;
+//	}
+//
+//	#if UNITY_IOS || UNITY_STANDALONE_OSX
+//	public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
+//	{
+//		extensions.GetExtension<IAppleExtensions> ().RestoreTransactions (result => {
+//			if (result) {
+//				// This does not mean anything was restored,
+//				// merely that the restoration process succeeded.
+//				ProcessPurchase(
+//			} else {
+//				// Restoration failed.
+//			}
+//		};
+//					}
+//	#endif
+//}
